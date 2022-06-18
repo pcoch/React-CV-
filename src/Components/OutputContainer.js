dd25d1c6cd43a05ae5f010f5dbc024a3af379b26
Paul Cochrane <paulstuartcochrane@gmail.com>
2022-06-18 22:36:56 +1000

    Icons Added

--- src/Components/OutputContainer.js
+++ src/Components/OutputContainer.js
@@ -1,10 +1,16 @@
 import React from "react";
 import OutputHeader from "./OutputHeader";
+import OutputWorkExperience from "./OutputWorkExperience";
+import OutputDescription from "./OutputDescription";
+import OutputEducation from "./OutputEducation";
 
 export default function OutputContainer() {
   return (
     <div className="OutputContainer">
       <OutputHeader />
+      <OutputDescription />
+      <OutputWorkExperience />
+      <OutputEducation />
     </div>
   );
 }
---
+++ src/Components/OutputEducation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function OutputEducation() {
+  return (
+    <div>
+      OutputEducation Cupidatat sunt anim incididunt nisi labore sunt nulla
+      Lorem elit irure. Aliquip quis excepteur et nostrud enim irure nostrud
+      officia. Et deserunt et aliquip voluptate elit cupidatat. Adipisicing enim
+      minim do anim eiusmod est. Irure laboris anim voluptate proident. Cillum
+      reprehenderit est magna minim. Nostrud ex aute laborum ea irure amet ea
+      ipsum ut non minim anim nisi.
+    </div>
+  );
+}
